9e816222b72c3c31796c206b0fa7ebeaaecb6126
Greg Chadwick <gac@lowrisc.org>
2024-09-02 15:24:13 +0100

    Update lowrisc_ip to lowRISC/opentitan@5ad6963fa7
    Update code from upstream repository
    https://github.com/lowRISC/opentitan to revision
    5ad6963fa71a63b4cc7817fb3bae5052c796bfc1
    
    Signed-off-by: Greg Chadwick <gac@lowrisc.org>

---
+++ hw/vendor/lowrisc_ip/ip/i2c/rtl/i2c.sv
@@ -0,0 +1,126 @@
+// Copyright lowRISC contributors (OpenTitan project).
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+//
+// Description: I2C top level wrapper file
+
+`include "prim_assert.sv"
+
+module i2c
+  import i2c_reg_pkg::*;
+#(
+  parameter int unsigned InputDelayCycles = 0
+) (
+  input                               clk_i,
+  input                               rst_ni,
+  input prim_ram_1p_pkg::ram_1p_cfg_t ram_cfg_i,
+
+  // Bus Interface
+  input  tlul_pkg::tl_h2d_t tl_i,
+  output tlul_pkg::tl_d2h_t tl_o,
+
+  // Generic IO
+  input                     cio_scl_i,
+  output logic              cio_scl_o,
+  output logic              cio_scl_en_o,
+  input                     cio_sda_i,
+  output logic              cio_sda_o,
+  output logic              cio_sda_en_o,
+
+  // Interrupts
+  output logic              intr_fmt_threshold_o,
+  output logic              intr_rx_threshold_o,
+  output logic              intr_acq_threshold_o,
+  output logic              intr_rx_overflow_o,
+  output logic              intr_controller_halt_o,
+  output logic              intr_scl_interference_o,
+  output logic              intr_sda_interference_o,
+  output logic              intr_stretch_timeout_o,
+  output logic              intr_sda_unstable_o,
+  output logic              intr_cmd_complete_o,
+  output logic              intr_tx_stretch_o,
+  output logic              intr_tx_threshold_o,
+  output logic              intr_acq_stretch_o,
+  output logic              intr_unexp_stop_o,
+  output logic              intr_host_timeout_o
+);
+
+  i2c_reg2hw_t reg2hw;
+  i2c_hw2reg_t hw2reg;
+
+  i2c_reg_top u_reg (
+    .clk_i,
+    .rst_ni,
+    .tl_i,
+    .tl_o,
+    .reg2hw,
+    .hw2reg
+  );
+
+  logic scl_int;
+  logic sda_int;
+
+  i2c_core #(
+    .InputDelayCycles(InputDelayCycles)
+  ) i2c_core (
+    .clk_i,
+    .rst_ni,
+    .ram_cfg_i,
+
+    .reg2hw,
+    .hw2reg,
+
+    .scl_i(cio_scl_i),
+    .scl_o(scl_int),
+    .sda_i(cio_sda_i),
+    .sda_o(sda_int),
+
+    .intr_fmt_threshold_o,
+    .intr_rx_threshold_o,
+    .intr_acq_threshold_o,
+    .intr_rx_overflow_o,
+    .intr_controller_halt_o,
+    .intr_scl_interference_o,
+    .intr_sda_interference_o,
+    .intr_stretch_timeout_o,
+    .intr_sda_unstable_o,
+    .intr_cmd_complete_o,
+    .intr_tx_stretch_o,
+    .intr_tx_threshold_o,
+    .intr_acq_stretch_o,
+    .intr_unexp_stop_o,
+    .intr_host_timeout_o
+  );
+
+  // For I2C, in standard, fast and fast-plus modes, outputs simulated as open-drain outputs.
+  // Asserting scl or sda high should be equivalent to a tri-state output.
+  // The output, when enabled, should only assert low.
+
+  assign cio_scl_o = 1'b0;
+  assign cio_sda_o = 1'b0;
+
+  assign cio_scl_en_o = ~scl_int;
+  assign cio_sda_en_o = ~sda_int;
+
+  `ASSERT_KNOWN(TlDValidKnownO_A, tl_o.d_valid)
+  `ASSERT_KNOWN(TlAReadyKnownO_A, tl_o.a_ready)
+  `ASSERT_KNOWN(CioSclKnownO_A, cio_scl_o)
+  `ASSERT_KNOWN(CioSclEnKnownO_A, cio_scl_en_o)
+  `ASSERT_KNOWN(CioSdaKnownO_A, cio_sda_o)
+  `ASSERT_KNOWN(CioSdaEnKnownO_A, cio_sda_en_o)
+  `ASSERT_KNOWN(IntrFmtWtmkKnownO_A, intr_fmt_threshold_o)
+  `ASSERT_KNOWN(IntrRxWtmkKnownO_A, intr_rx_threshold_o)
+  `ASSERT_KNOWN(IntrAcqWtmkKnownO_A, intr_acq_threshold_o)
+  `ASSERT_KNOWN(IntrRxOflwKnownO_A, intr_rx_overflow_o)
+  `ASSERT_KNOWN(IntrControllerHaltKnownO_A, intr_controller_halt_o)
+  `ASSERT_KNOWN(IntrSclInterfKnownO_A, intr_scl_interference_o)
+  `ASSERT_KNOWN(IntrSdaInterfKnownO_A, intr_sda_interference_o)
+  `ASSERT_KNOWN(IntrStretchTimeoutKnownO_A, intr_stretch_timeout_o)
+  `ASSERT_KNOWN(IntrSdaUnstableKnownO_A, intr_sda_unstable_o)
+  `ASSERT_KNOWN(IntrCommandCompleteKnownO_A, intr_cmd_complete_o)
+  `ASSERT_KNOWN(IntrTxStretchKnownO_A, intr_tx_stretch_o)
+  `ASSERT_KNOWN(IntrTxWtmkKnownO_A, intr_tx_threshold_o)
+  `ASSERT_KNOWN(IntrAcqStretchKnownO_A, intr_acq_stretch_o)
+  `ASSERT_KNOWN(IntrUnexpStopKnownO_A, intr_unexp_stop_o)
+  `ASSERT_KNOWN(IntrHostTimeoutKnownO_A, intr_host_timeout_o)
+endmodule
