9d45ead09478e7f61e5a6a30122efc2babac926a
Adrian Lees <a.lees@lowrisc.org>
2024-11-15 18:39:23 +0000

    Extend chip_check with simple pwm and pattgen testing
    Configure the pwm and pattgen IP blocks to produce test signals
    that may be inspected in simulation; these outputs are not
    available for programmatic testing presently.
    
    Report register values on the UART to confirm successful write/
    read access to the IP block registers over TL-UL.
    The signals have been inspected in xcelium simulation in
    top_chip_asic. Presently they are not exported via pads because
    these signals will surely need multiplexing onto pins for any
    real implementation.

--- scratch_sw/bare_metal/checks/chip_check.cc
+++ scratch_sw/bare_metal/checks/chip_check.cc
@@ -5,6 +5,8 @@
 #include "../common/defs.h"
 #include <cheri.hh>
 #include "../common/platform-gpio.hh"
+#include "../common/platform-pattgen.hh"
+#include "../common/platform-pwm.hh"
 #include "../common/uart-utils.hh"
 #include <stdint.h>
 
@@ -28,6 +30,14 @@ extern "C" void rom_loader_entry(void *rwRoot)
   gpio.address() = GPIO_ADDRESS;
   gpio.bounds()  = GPIO_BOUNDS;
 
+  Capability<volatile OpenTitanPattgen> pattgen = root.cast<volatile OpenTitanPattgen>();
+  pattgen.address() = PATTGEN_ADDRESS;
+  pattgen.bounds()  = PATTGEN_BOUNDS;
+
+  Capability<volatile OpenTitanPwm> pwm = root.cast<volatile OpenTitanPwm>();
+  pwm.address() = PWM_ADDRESS;
+  pwm.bounds()  = PWM_BOUNDS;
+
   uart->init(BAUD_RATE);
 
   uint32_t next_out = 0xAAAAAAAA;
@@ -41,6 +51,45 @@ extern "C" void rom_loader_entry(void *rwRoot)
     write_hex8b(uart, i);
     write_str(uart, "\r\n");
   }
+
+  // Set up pattgen to emit a couple of distinct test patterns, with different clock polarities.
+  pattgen->prediv_ch0 = 2;
+  pattgen->prediv_ch1 = 3;
+  pattgen->data_ch0_0 = 0xaaaaaaaau;
+  pattgen->data_ch0_1 = 0xf0f0f0f0u;
+  pattgen->data_ch1_0 = 0x33333333u;
+  pattgen->data_ch1_1 = 0x11111111u;
+  // Emit each pattern for as long as possible.
+  pattgen->size = (0x3ffu << 22) | (0x3fu << 16) | (0x3ffu << 6) | 0x3fu;
+
+  write_str(uart, "Generating pattern\r\n");
+  // Enable pattern output.
+  pattgen->ctrl = 0x6b;
+
+  // Set up pwm to emit a different pattern on each of its outputs.
+  pwm->cfg = 0xf0000001u;
+  pwm->invert = 0x2a;
+  for (unsigned ch = 0u; ch < PWM_NUM_OUTPUTS; ch++) {
+    pwm->pwm_param[ch]   = ch << 12;
+    pwm->duty_cycle[ch]  = (7 - ch) << 12;
+    pwm->blink_param[ch] = 0u;
+  }
+
+  write_str(uart, "Enabling PWM\r\n");
+  pwm->pwm_en = 0x3f;
+
+  // Just report some register values since we cannot see the outputs of these IP blocks.
+  write_str(uart, "Pattgen channel 0 : ");
+  write_hex(uart, pattgen->data_ch0_1);
+  write_str(uart, " : ");
+  write_hex(uart, pattgen->data_ch0_0);
+  write_str(uart, "\r\n");
+
+  write_str(uart, "PWM output 0 : ");
+  write_hex(uart, pwm->pwm_param[0]);
+  write_str(uart, " : ");
+  write_hex(uart, pwm->duty_cycle[0]);
+  write_str(uart, "\r\n");
 
   // Wait for UART idle
   while (!(uart->status & 0x8));
