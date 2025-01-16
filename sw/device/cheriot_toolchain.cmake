ed57fc3fb0533b1b61a4bd822652508e1c4bad01
Adrian Lees <a.lees@lowrisc.org>
2025-01-16 17:10:58 +0000

    Build files and bootstrap code
    Software build files for cmake, maintaining the existing Sonata-style
    flow. Linker script for a single code image that executes from RAM.
    Bootstrap code that set ups capabilities for pointers in global
    statics.

---
+++ sw/device/cheriot_toolchain.cmake
@@ -0,0 +1,40 @@
+set(LINKER_SCRIPT "${CMAKE_CURRENT_LIST_DIR}/link.ld")
+
+set(CMAKE_SYSTEM_NAME Generic)
+
+if (DEFINED ENV{CHERIOT_LLVM_BIN})
+  set(CMAKE_CXX_COMPILER "$ENV{CHERIOT_LLVM_BIN}/clang++")
+  set(CMAKE_C_COMPILER   "$ENV{CHERIOT_LLVM_BIN}/clang")
+  set(CMAKE_ASM_COMPILER "$ENV{CHERIOT_LLVM_BIN}/clang")
+  set(CMAKE_OBJCOPY      "$ENV{CHERIOT_LLVM_BIN}/llvm-objcopy")
+else()
+  set(CMAKE_CXX_COMPILER clang++)
+  set(CMAKE_C_COMPILER   clang)
+  set(CMAKE_ASM_COMPILER clang)
+  set(CMAKE_OBJCOPY      llvm-objcopy)
+endif()
+
+set(CMAKE_ASM_COMPILER_TARGET riscv32-unknown-unknown)
+set(CMAKE_C_COMPILER_TARGET   riscv32-unknown-unknown)
+set(CMAKE_CXX_COMPILER_TARGET riscv32-unknown-unknown)
+
+set(CMAKE_CXX_STANDARD 20)
+set(CMAKE_CXX_STANDARD_REQUIRED TRUE)
+
+string(CONCAT CMAKE_CXX_FLAGS_INIT
+  "-std=c++20 -mcpu=cheriot -mabi=cheriot-baremetal -mxcheri-rvc -mrelax -fshort-wchar"
+  " -Os -g -fomit-frame-pointer -fno-builtin -fno-exceptions"
+  " -fno-asynchronous-unwind-tables -fno-c++-static-destructors -fno-rtti"
+  " -Wall -fvisibility=hidden -fvisibility-inlines-hidden"
+  " -DDEVICE_EXISTS_uart"
+)
+
+string(CONCAT CMAKE_C_FLAGS_INIT
+  "-mcpu=cheriot -mabi=cheriot-baremetal -mxcheri-rvc -mrelax -fshort-wchar"
+  " -Os -g -fomit-frame-pointer -fno-builtin -Wall -Werror"
+  " -fno-asynchronous-unwind-tables -fvisibility=hidden -fvisibility-inlines-hidden"
+)
+
+set(CMAKE_ASM_FLAGS_INIT "-mcpu=cheriot -mabi=cheriot -mxcheri-rvc -mrelax -fshort-wchar")
+
+set(CMAKE_EXE_LINKER_FLAGS_INIT "-nodefaultlibs -fuse-ld=lld -T \"${LINKER_SCRIPT}\"")
---
+++ sw/device/lib/boot/boot.c
@@ -0,0 +1,53 @@
+// Copyright lowRISC contributors (Sunburst project).
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#include <stdint.h>
+
+// TODO: Problem with CLLC; the cincaddrimm appears to add the wrong offset, not compensating
+// properly for the fact that auipcc has a smaller offset shift.
+#include "cheri_init_globals.h"
+
+#include "sw/device/lib/base/mmio.h"
+#include "sw/device/lib/base/memory.h"
+
+// TODO: Set root capability for all DIF access.
+extern volatile void *__capability mmioRoot;
+
+void boot_init_globals(void *__capability gdc, uint32_t start_addr, uint32_t stop_addr) {
+  const void *__capability code_cap = __builtin_cheri_program_counter_get();
+  const void *__capability rodata_cap = gdc;
+  void *__capability data_cap = gdc;
+
+  const struct capreloc *start_relocs = __builtin_cheri_address_set(gdc, start_addr);
+  const struct capreloc *stop_relocs  = __builtin_cheri_address_set(gdc, stop_addr);
+
+  // This code is derived from that in cheri_init_globals.h but it retains full permissions
+  // without trying to lock down the protection model.
+  for (const struct capreloc *reloc = start_relocs; reloc < stop_relocs;
+       reloc++) {
+    const void *__capability *__capability dest =
+        (const void *__capability *__capability)cheri_address_or_offset_set(
+            data_cap, reloc->capability_location);
+    if (reloc->object == 0) {
+      /* XXXAR: clang fills uninitialized capabilities with 0xcacaca..., so we
+       * we need to explicitly write NULL here */
+      *dest = (void *__capability)0;
+      continue;
+    }
+    const void *__capability base_cap;
+    if ((reloc->permissions & function_reloc_flag) == function_reloc_flag) {
+      base_cap = code_cap; /* code pointer */
+    } else if ((reloc->permissions & constant_reloc_flag) ==
+               constant_reloc_flag) {
+      base_cap = rodata_cap; /* read-only data pointer */
+    } else {
+      base_cap = data_cap; /* read-write data */
+    }
+    const void *__capability src = cheri_address_or_offset_set(base_cap, reloc->object);
+    *dest = __builtin_cheri_offset_increment(src, reloc->offset);
+  }
+
+  // TODO: set up MMIO accesss.
+  mmioRoot = gdc;
+}
